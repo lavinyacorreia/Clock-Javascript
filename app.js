ebb2937f8eddcd5d8d700803163d4618e33835ab
Lavinya Correia <lavinya.correia01@gmail.com>
2021-04-03 15:47:22 -0300

    Javascript clock project - first step

---
+++ app.js
@@ -0,0 +1,22 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+  
+    //hora, min, sec com 2 casas, como "02":"04":"08"
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
